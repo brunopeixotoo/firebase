08bc9c3c57d5f35894bb9979b9232ef5e669c582
Bruno Peixoto <brunopeixotorodrigues546@gmail.com>
2024-04-18 13:34:26 -0400

    firebase

--- firebase-training/src/App.js
+++ firebase-training/src/App.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
 import { db } from './firebaseConnection';
-import { doc, setDoc, collection, addDoc, getDoc} from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc} from 'firebase/firestore';
 
 import './App.css';
 
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+  const [idpost, setIdPost] = useState('');
 
-
+  const [posts, setPosts] = useState([]);
 
   async function handleAdd() {
   
@@ -29,26 +30,63 @@ function App() {
   }
 
   async function buscaPost() {
-    
-    const postRef = doc(db, "posts", "0tCDFtP4mNE2jjoTsdnC");
 
-    await getDoc(postRef)
+    const postRef = collection(db, "posts")
+    await getDocs(postRef)
     .then((snapshot)=>{
-      setTitulo(snapshot.data().titulo);
-      setAutor(snapshot.data().autor);
-    })
-    .catch((error)=>{
+      let lista = []
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          autor: doc.data().autor,
+          titulo: doc.data().titulo
+        })
+      })
+
+      setPosts(lista);
+
+    }).catch((error)=>{
       console.log('[function buscaPost] Error: ', error);
     })
 
+  }
+
+  async function updateData() {
+    
+    const docRef = doc(db, "posts", idpost);
+
+    await updateDoc(docRef, {
+      titulo: titulo,
+      autor: autor
+    })
+    .then(() => {
+      console.log('Update sucess');
+      setIdPost('');
+      setTitulo('');
+      setAutor('');
+    })
+    .catch((error) => {
+      console.log('[function updateData] Error: ', error)
+    })
 
   }
+
 
   
   return (
     <div>
       <h1>React + Firebase</h1>
       <div className='container'>
+
+        <label>ID do Post: </label>
+        <input
+        type='text'
+        placeholder='Ex: 0tCDFtP4mNE2jjoTsdnC'
+        value={idpost}
+        onChange={(e)=> setIdPost(e.target.value)}
+        />
+
         <label>Título</label>
         <input 
         type='text'
@@ -67,6 +105,19 @@ function App() {
 
         <button onClick={handleAdd}>Cadastrar</button>
         <button onClick={buscaPost}>Buscar posts</button>
+        <button onClick={updateData}>Atualizar post</button>
+
+        <ul className='list'>
+            {posts.map( (post) =>{
+              return(
+                <li key={post.id}>
+                  <strong>ID: {post.id}</strong> <br/>
+                  <span>Título: {post.titulo}</span> <br/>
+                  <span>Autor: {post.autor}</span>
+                </li>
+              )
+            })}
+        </ul>
       </div>
 
     </div>
